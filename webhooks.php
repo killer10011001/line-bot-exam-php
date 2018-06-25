97f5440275a8f20e587e46d12545c3371a5d7115
killer10011001 <40558427+killer10011001@users.noreply.github.com>
2018-06-25 14:26:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'So71sGllfliZdAkHDxAUhw/vIzRfmdwXmYHX72+l4/8X0VXUgBoQyr7ofYxof6DkCFrjig5F9zl+lSdAesXLGbatN3DB4lfLd7AqD+kjzjLAQGhechp4JPaNsd3tYCu5QXI7afFKom+az+pKwuetfQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
